16fc7df7497c77aba4ddd566a6b457336d9cc6eb
南志於 <au341998@gmail.com>
2019-10-19 14:54:57 +0900

    初回コミット_5

---
+++ Assets/Script/JyunbiPopUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JyunbiPopUp : MonoBehaviour
+{
+    
+    public StagePopUp stagePopUpPrefab;
+    //public GameObject stagePopUpPrefab;
+    public Transform canvasTransform;
+
+
+    public void CreatePopUp(string kinName)
+    {
+        //ゲームオブジェクト型でインスタンシエイトすると欲しい情報をわざわざGetcomponentしないといけなくなるが
+        //StagePopUp stagePopUp =
+        //GameObject test = Instantiate(stagePopUpPrefab, canvasTransform, false);
+        //StagePopUp stagePopUp = test.GetComponent<StagePopUp>();
+        //stagePopUp.kinImage = ;
+
+        //クラスでクローンするとそのままクラスに入れられる、欲しい情報を直でもらえる
+        //第一引数には欲しいクラスがついているプレファブを指定する。prefabの型と欲しいクラスを合わせる。
+        //クラスをprefabに指定してもちゃんとゲームオブジェクトができる。
+        StagePopUp stagePopUp = Instantiate(stagePopUpPrefab, canvasTransform, false);
+        //kinImageはpulicで宣言されているのでいじれる
+        stagePopUp.kinImage.sprite = Resources.Load<Sprite>("Image/" + kinName);
+
+    }
+}
---
+++ Assets/Script/StagePopUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class StagePopUp : MonoBehaviour
+{
+    public GameObject backGround;
+    public Image kinImage;
+
+    public void ClosePopUp()
+    {
+        //シーケンス宣言して、Appendで順番に処理を書いていく、秒数の合計はDestroyの処理時間に合わせる
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(backGround.transform.DOScale(new Vector3(1.5f, 1.5f, 1.5f), 0.15f).SetEase(Ease.Linear));
+        sequence.Append(backGround.transform.DOScale(new Vector3(0.5f, 0.5f, 0.5f), 0.5f).SetEase(Ease.Linear));
+                sequence.Join(backGround.GetComponent<CanvasGroup>().DOFade(0, 0.5f).SetEase(Ease.Linear));
+
+
+        //backGround.transform.DOScale(new Vector3(0.5f, 0.5f, 0.5f), 1.0f);
+        //第二引数にfloat型で破壊されるまでの待ち時間を指定できる
+        Destroy(gameObject, 0.65f);
+    }
+
+
+}
